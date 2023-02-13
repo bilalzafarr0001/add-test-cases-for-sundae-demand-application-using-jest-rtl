8aa6ac970357179e8c1e7539f326c6cfdcd9db0e
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2023-02-13 21:18:19 +0500

    Finalize test cases

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constants";
+import { formatCurrency } from "../../utilities";
+import { useOrderDetails } from "../../contexts/OrderDetails";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+  const { totals } = useOrderDetails();
+
+  useEffect(() => {
+    // create an abortController to attach to the network request
+    const controller = new AbortController();
+    axios
+      // attach abortController to request
+      .get(`http://localhost:3030/${optionType}`, {
+        signal: controller.signal,
+      })
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        if (error.name !== "CanceledError") {
+          setError(true);
+        }
+      });
+    return () => {
+      // on unmount, abort any active requests
+      controller.abort();
+    };
+  }, [optionType]);
+
+  if (error) {
+    // @ts-ignore
+    return <AlertBanner />;
+  }
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{formatCurrency(pricePerItem[optionType])} each</p>
+      <p>
+        {title} total: {formatCurrency(totals[optionType])}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
+}
